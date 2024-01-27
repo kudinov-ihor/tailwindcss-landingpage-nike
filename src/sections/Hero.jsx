9ce7cd8cd81332bdb83f75790e393cb4634f7c9d
kudinov-ihor <kudinov.igor98@gmail.com>
2024-01-27 17:04:41 +0200

    added section popularProducts

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
         </h1>
         <p className="font-motserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
           Discover stylish Nike arrivals, quality comfort, and innovation for
-          your active life.
+          your active life.Life is good
         </p>
         <Button label="Shop Now" iconURL={arrowRight} />
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
